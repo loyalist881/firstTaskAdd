2e335fd9a42137a2486b5fbba08ce1eaac32ee8b
loyalist881 <sokolovpavel31@gmail.com>
2025-09-17 15:43:37 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int income = 44000;
+        int spending = 30000;
+        System.out.println("Итого (руб):");
+        System.out.println(income - spending);
+    }
+}
